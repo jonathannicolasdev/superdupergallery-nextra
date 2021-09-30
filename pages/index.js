cc86d7d3794e1cdf52c445b0091271d961d5447e
M Haidar Hanif <git@mhaidarhanif.com>
2021-09-30 21:01:13 +0700

    feat: layout page and article

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import NextHead from 'next/head'
 import { styled } from '../stitches.config'
 
+const Page = styled('main', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '1em'
+})
+
 const Header = styled('header', {
-  margin: '1em',
+  width: '100%',
   border: '2px solid white',
   minHeight: '100px',
   display: 'flex',
@@ -64,7 +71,7 @@ const MenuButton = styled('button', {
 const Hero = styled('section', {
   display: 'flex',
   justifyContent: 'center',
-  paddingTop: '75px'
+  margin: '75px 0'
 })
 
 const HeroCenter = styled('div', {})
@@ -87,9 +94,31 @@ const HeroHeading = styled('h1', {
   }
 })
 
+const Article = styled('article', {
+  color: 'white',
+  margin: '1em',
+  padding: '1em 0',
+  fontSize: '1.2rem',
+  maxWidth: '720px'
+})
+
+const ArticleHeading = styled('h3', {
+  textTransform: 'uppercase',
+  fontWeight: 900,
+  fontSize: '2rem',
+  maxWidth: '24ch'
+})
+
+const Section = styled('section', {
+  marginBottom: '1em',
+  p: {
+    marginBottom: '0.25em'
+  }
+})
+
 export default function HomePage() {
   return (
-    <div>
+    <Page>
       <NextHead>
         <title>Super Duper Gallery</title>
       </NextHead>
@@ -121,6 +150,57 @@ export default function HomePage() {
           </HeroHeading>
         </HeroCenter>
       </Hero>
-    </div>
+
+      <Article>
+        <Section>
+          <ArticleHeading>
+            Super Duper Gallery offers a portal further beyond.
+          </ArticleHeading>
+        </Section>
+        <Section>
+          <p>
+            It is a contemporary art gallery that welcomes a wide range of
+            artistic provocations.
+          </p>
+          <p>
+            Artists are given the opportunity to explore and creatively labor
+            upon the energy that drives their limitless imagination.
+          </p>
+          <p>
+            The gallery embraces a range of expressions and intentions, from
+            serious to the unapologetically whimsical.
+          </p>
+        </Section>
+        <Section>
+          <p>
+            Artists may freely choose the contemplative in challenging yet
+            innovative ways or provide an occasional jesting commentary.
+          </p>
+          <p>
+            They can also renegotiate ontologies of art and rethink parameters
+            of validity in the market.
+          </p>
+        </Section>
+        <Section>
+          <p>
+            Although it is physically located in Quezon City, the gallery offers
+            a glimpse into the diverse contemporary art practices and media that
+            artists have to offer regardless of ethnicity, nationality, and
+            geographic borders.
+          </p>
+        </Section>
+        <Section>
+          <p>
+            Super Duper is a contemporary art gallery that features carefully
+            curated exhibitions that transcend conceptual boundaries,
+            established trends, and cliques.
+          </p>
+          <p>
+            The innovative program of exhibitions is reflective of the gallery’s
+            dictum: ignore the compulsory and pursue the super duper.
+          </p>
+        </Section>
+      </Article>
+    </Page>
   )
 }
